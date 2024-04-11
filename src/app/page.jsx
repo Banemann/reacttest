6e3f3980c9bcf013172348a233f9d4e00a2c09c8
Banemann <casperbanemann@gmail.com>
2024-04-11 10:58:25 +0200

    components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,5 @@
+import link from "next/link";
+
 export const metadata = {
   title: "Home",
   description: "Home page",
@@ -7,7 +9,8 @@ export const metadata = {
 export default function Home() {
   return (
     <main >
-      
+      <h1>dsds</h1>
+      <link rel="stylesheet" href="" prefetch="false"/>
     </main>
   );
 }
